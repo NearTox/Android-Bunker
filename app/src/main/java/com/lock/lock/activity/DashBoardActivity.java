3ec851c0f4c630be45b6094720dc02b6a6c6c182
NearTox <NearTox@outlook.com>
2016-08-03 13:36:14 -0500

    added recyclerview and dummy cardview

--- app/src/main/java/com/lock/lock/activity/DashBoardActivity.java
+++ app/src/main/java/com/lock/lock/activity/DashBoardActivity.java
@@ -3,20 +3,35 @@ package com.lock.lock.activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.lock.lock.R;
+import com.lock.lock.recycler.CalendarAdapter;
 
-public class DashBoardActivity extends AppCompatActivity {
+public class DashBoardActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
 
-  FirebaseUser user;
+  private RecyclerView mCalendarList;
+  private CalendarAdapter mCalendarAdapter;
+  private DrawerLayout drawerLayout;
+
+  private FirebaseUser user;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -24,6 +39,7 @@ public class DashBoardActivity extends AppCompatActivity {
 
     Fresco.initialize(this);
     user = FirebaseAuth.getInstance().getCurrentUser();
+    drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
 
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
@@ -36,5 +52,51 @@ public class DashBoardActivity extends AppCompatActivity {
             .setAction("Action", null).show();
       }
     });
+    final ActionBar ab = getSupportActionBar();
+    ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+    ab.setDisplayHomeAsUpEnabled(true);
+
+    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
+
+
+    mCalendarList = (RecyclerView) findViewById(R.id.my_calendar_list);
+    mCalendarList.setLayoutManager(new LinearLayoutManager(this));
+    mCalendarAdapter = new CalendarAdapter(this);
+    mCalendarList.setAdapter(mCalendarAdapter);
+  }
+
+  @Override
+  public void onBackPressed() {
+    if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+      drawerLayout.closeDrawer(GravityCompat.START);
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        drawerLayout.openDrawer(GravityCompat.START);
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  @SuppressWarnings("StatementWithEmptyBody")
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item) {
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+    if(id == R.id.nav_logout) {
+      FirebaseAuth.getInstance().signOut();
+      Intent main = new Intent(this, MainActivity.class);
+      startActivity(main);
+      finish();
+    }
+    drawerLayout.closeDrawer(GravityCompat.START);
+    return true;
   }
 }
---
+++ app/src/main/java/com/lock/lock/model/CalendarModel.java
@@ -0,0 +1,7 @@
+package com.lock.lock.model;
+
+public class CalendarModel {
+  public String Day;
+  public String Month;
+  public String Year;
+}

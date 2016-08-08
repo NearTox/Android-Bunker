1853b40509515a138146ec2f202a00c1c21a5f8f
NearTox <NearTox@outlook.com>
2016-08-08 14:53:00 -0500

    add new form[xml only]

--- app/src/main/java/com/lock/lock/activity/AddNewActivity.java
+++ app/src/main/java/com/lock/lock/activity/AddNewActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.lock.lock.R;
@@ -19,15 +20,27 @@ public class AddNewActivity extends AppCompatActivity {
 
     setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+    getSupportActionBar().setTitle("Nueva Poliza");
     drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
   }
 
   @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_add_new, menu);
+    return true;
+  }
+
+  @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-    if(item.getItemId()== android.R.id.home){
-        finish();
-        return true;
+    int id = item.getItemId();
+
+    if(id == android.R.id.home) {
+      finish();
+      return true;
+    } else if(id == R.id.menu_add_new_descartar) {
+
+      return true;
     }
     return super.onOptionsItemSelected(item);
   }

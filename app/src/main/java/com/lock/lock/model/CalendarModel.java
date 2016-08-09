acac2693a131388625c09cb1bd491bf8e6644644
NearTox <NearTox@outlook.com>
2016-08-08 21:53:29 -0500

    pre firebase realtime

--- app/src/main/java/com/lock/lock/model/CalendarModel.java
+++ app/src/main/java/com/lock/lock/model/CalendarModel.java
@@ -3,5 +3,14 @@ package com.lock.lock.model;
 public class CalendarModel {
   public String Day;
   public String Month;
-  public String Year;
+
+  public String NoPoliza;
+  public String Name;
+  public String Phone;
+  public String Email;
+  public boolean IsPreferenctial;
+  public String Company;
+  public int Plan;
+
+
 }
